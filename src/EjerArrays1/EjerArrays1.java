68deacfe2f8e8e3fd992ced336264ec1a7584f35
Elias-N-F <feliasnataniel@gmail.com>
2019-08-09 11:36:01 -0300

    Update EjerArrays1.java

--- src/EjerArrays1/EjerArrays1.java
+++ src/EjerArrays1/EjerArrays1.java
@@ -16,11 +16,9 @@ public class EjerArrays1 {
     public static void main (String[] args){
     
     String nombres[]= new String[4];
-    nombres[0]="Juancho";
-    nombres[1]="Juan";
-    nombres[2]="Juance";
-    nombres[3]="Juan carlos";
+
     for (int i=0;i<4;i++){nombres[i]=JOptionPane.showInputDialog("Ingrese su nombre");}
+
     for(String nombre:nombres){System.out.println(nombre);}
     
     
